45d0a40591d56ecff86abe201e4be77978fc3f46
Jack Chen <jack.chen@databricks.com>
2023-07-19 09:12:54 +0800

    [SPARK-44448][SQL] Fix wrong results bug from DenseRankLimitIterator and InferWindowGroupLimit
    Top-k filters on a dense_rank() window function return wrong results, due to a bug in optimization InferWindowGroupLimit, specifically in the code for DenseRankLimitIterator, introduced in https://issues.apache.org/jira/browse/SPARK-37099.
    
    The bug is in DenseRankLimitIterator, it fails to reset state properly when transitioning from one window partition to the next. reset only resets rank = 0, what it is missing is to reset currentRankRow = null. This means that when processing the second and later window partitions, the rank incorrectly gets incremented based on comparing the ordering of the last row of the previous partition to the first row of the new partition.
    
    This means that a dense_rank window func that has more than one window partition and more than one row with dense_rank = 1 in the second or later partitions can give wrong results when optimized.
    
    RankLimitIterator narrowly avoids this bug by happenstance, the first row in the new partition will try to increment rank, but increment it by the value of count which is 0, so it happens to work by accident. This PR also fixes the reset function in RankLimitIterator to make it more robust.
    
    Example repro:
    ```
    create or replace temp view t1 (p, o) as values (1, 1), (1, 1), (1, 2), (2, 1), (2, 1), (2, 2);
    
    select * from (select *, dense_rank() over (partition by p order by o) as rnk from t1) where rnk = 1;
    ```
    
    Spark result:
    ```
    [1,1,1]
    [1,1,1]
    [2,1,1]
    ```
    
    Correct result:
    ```
    [1,1,1]
    [1,1,1]
    [2,1,1]
    [2,1,1]
    ```
    
    Fix wrong results bug.
    
    Yes, fixes wrong results.
    
    Add sql tests and unit tests.
    
    Unfortunately, the previous tests for the optimization only had a single row per rank, so did not catch the bug as the bug requires multiple rows per rank. This PR strengthens the tests with data that contains multiple rows per rank.
    
    Closes #42026 from jchen5/dense-rank-limit.
    
    Authored-by: Jack Chen <jack.chen@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/window/WindowGroupLimitExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/window/WindowGroupLimitExec.scala
@@ -173,6 +173,7 @@ case class RankLimitIterator(
   override def reset(): Unit = {
     rank = 0
     count = 0
+    currentRankRow = null
   }
 }
 
@@ -196,6 +197,7 @@ case class DenseRankLimitIterator(
 
   override def reset(): Unit = {
     rank = 0
+    currentRankRow = null
   }
 }
 
